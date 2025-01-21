b2f08cca893d877c940e94f55c5f58b333180dd9
madame-r <choufies@icloud.com>
2025-01-21 16:50:45 +0100

    updating userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,25 +1,15 @@
-
 const express = require('express');
 const { registerUser, loginUser } = require('../controllers/userController');
 
-
 const router = express.Router();
 
-
-
-// Route d'inscription
-// router.post('/register', registerUser);
-
+// Middleware pour la route d'inscription (pour loguer les requêtes)
 router.post('/register', (req, res, next) => {
     console.log('Received registration request:', req.body); // Log de la requête
-    next(); // Passe au contrôleur
+    next(); // Passe au contrôleur d'inscription
 }, registerUser);
 
-
-
 // Route de connexion
-router.post('/login',loginUser);
-
-
+router.post('/login', loginUser);
 
 module.exports = router;
